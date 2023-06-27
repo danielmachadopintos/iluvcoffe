f89a82a6d8dc728f680bba8ddb4af99f1e18840b
Daniel Machado <danielmachadopintos@hotmail.com>
2023-06-26 22:49:19 -0300

    feat: add dynamic config import

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,18 +19,20 @@ import appConfig from './config/app.config';
       load: [appConfig],
     }), // carrega as variáveis de ambiente
     CoffeesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres', // tipo do banco de dados
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true, // carrega as entidades automaticamente
-      synchronize: true,
-      // sincroniza as entidades com o banco de dados,
-      // não é recomendado para PRODUÇAO,
-      // porque ele faz é dropar as tabelas e criar novamente
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres', // tipo do banco de dados
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: true, // carrega as entidades automaticamente
+        synchronize: true,
+        // sincroniza as entidades com o banco de dados,
+        // não é recomendado para PRODUÇAO,
+        // porque ele faz é dropar as tabelas e criar novamente
+      }),
     }),
     CoffeeRatingModule,
   ],
