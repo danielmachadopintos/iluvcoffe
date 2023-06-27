7cff01064619e8477f986d490b5ac3dbf6bb5829
Daniel Machado <danielmachadopintos@hotmail.com>
2023-06-26 22:44:02 -0300

    feat: add module constants partial registration

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,16 +9,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 // importação do modulo coffees
 import { CoffeesModule } from './coffees/coffees.module';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
-import * as Joi from '@hapi/joi';
+import appConfig from './config/app.config';
+
+// load
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env', // caminho do arquivo de variáveis de ambiente
-      validationSchema: Joi.object({
-        DATABASE_HOST: Joi.required(),
-        DATABASE_PORT: Joi.number().default(5432),
-      }), // schema de validação das variáveis de ambiente
+      load: [appConfig],
     }), // carrega as variáveis de ambiente
     CoffeesModule,
     TypeOrmModule.forRoot({
--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,9 +6,13 @@ import { Coffee } from './entities/coffe.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from 'src/events/entities/event.entity';
 import { ConfigModule } from '@nestjs/config';
+import coffeesConfig from './config/coffees.config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event]), ConfigModule], // 👈 Adding Coffee Entity here to the array
+  imports: [
+    TypeOrmModule.forFeature([Coffee, Flavor, Event]),
+    ConfigModule.forFeature(coffeesConfig), // partial registration
+  ], // 👈 Adding Coffee Entity here to the array
   controllers: [CoffesController],
   providers: [CoffeesService],
   exports: [CoffeesService],
